01cbfd1196bef6869ad9393ed804992206d081ef
apoorva-19 <apoorva19.am@gmail.com>
2020-12-20 14:34:48 +0530

    Forms added

--- food/models.py
+++ food/models.py
@@ -7,4 +7,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=20)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
-    
+    item_image = models.CharField(max_length=500, default="https://lh3.googleusercontent.com/proxy/XY2-s8N-sBqJVpUns28STtU0Y3B_Nygv3dE4W3I5KX_RIAibk63DtSGrEv-1Lk5Aitg-dEgUpnoGYge93n4euRZZTQc2rxODrhNZna3FCCCJ0W2DctKGVBf5c3tW")
--- food/views.py
+++ food/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Item
+from .forms import ItemForm
 
 def index(request):
     item_list = Item.objects.all()
@@ -22,3 +23,30 @@ def detail(request, item_id):
     }
     return render(request, 'food/detail.html', context)
     # return HttpResponse("This is item number: %s" % item_id)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'food/item_form.html', {'form':form})
+
+def update_item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'food/item_form.html', {'form':form, 'item':item})
+
+def delete_item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    if request.method == "POST":
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request, 'food/item_delete.html', {'item':item})
